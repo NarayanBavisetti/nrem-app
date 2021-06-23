da6d9e3b5961a9adc579a19d006a6ca38c6112bd
narayan <narayan3119@gmail.com>
2021-06-23 21:34:50 +0530

    setuped basic server

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const dotenv = require("dotenv")
+const PORT = process.env.PORT || 5000;
+
+dotenv.config({path : "./.env"});
+require("./db/connection")
+const middleware = (req, res, next) => {
+  console.log("Hi middle ware");
+  next();
+};
+
+
+// middleware();
+app.get("/", (req, res) => {
+  res.send("Hi welcome to home");
+});
+
+app.get("/about", middleware, (req, res) => {
+  res.send("Hi welcome to about page");
+});
+app.listen(PORT, () => console.log(`server is running at ${PORT}`));
